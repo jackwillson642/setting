e32b39447099c785af6a14d228233b899423d075
jackwillson642 <jackwillson642@gmail.com>
2021-08-04 11:38:30 +0530

    Absolute path in program removed

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
 import shutil
 
-listfiles = os.scandir('./settings/')
+# exefile = input("Enter path to the exe:\n")
+# source = os.path.split(exefile)[1]
+# settings = input("Enter path to folder containing textfiles:\n")
+
+exefile = "/home/jack/programming/python/setting/src/test.exe"
+source = os.path.split(exefile)[0]
+settings = "/home/jack/programming/python/setting/settings/"
+
+listfiles = os.scandir(settings)
 
 # testfiles is a list containing all the txt files in the settings directory
 textfiles = []
@@ -9,18 +17,11 @@ for file in listfiles:
     if file.is_file() and file.name.split('.')[-1] == "txt":   # Checks if the contents of the directory is a file and has a txt extention
         textfiles.append(file.name)
 
-
-# exefile = input("Enter path to the exe:\n")
-# source = os.path.split(exefile)[1]
-# settings = input("Enter path to folder containing textfiles:\n")
-
-exefile = "/home/jack/programming/python/new_project/src/test.exe"
-source = os.path.split(exefile)[0]
-settings = "/home/jack/programming/python/new_project/settings/"
-
 for filename in textfiles:
     src = os.path.join(settings, filename)
     dest = os.path.join(source, filename)
     shutil.copyfile(src, dest)  # Copy the text file to the source directory
-    print(src + " copied to " + dest)
+    print(f"{src} copied to {dest}\n")
     os.system(exefile)  # Execute the program
+
+input("Press Enter to exit")
